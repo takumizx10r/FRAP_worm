b488f004651aff3329d3410704f42e750a844262
TakumiSaito <TakumiSaito@DESKTOP-G61LOBD>
2022-10-13 11:59:56 +0900

    Newly updated

--- FRAPAnalysis.m
+++ FRAPAnalysis.m
@@ -2,20 +2,27 @@ clear
 close all
 
 
-
 [inputimage, path]=uigetfile(strcat(pwd,'\.tif'));
+listfile=dir(strcat(path,'00*.tif'));
 cd (path)
-tiff_info = imfinfo(inputimage);
 
 prompt = {'Frame interval (s):','Prebleach frames:','Right after bleach frame:','Fit range:'};
 dlgtitle = 'Input';
 dims = [1 35];
-definput = {'0.0884','2','3','30'};
+definput = {'0.0884','2','3','inf'};
 answer = inputdlg(prompt,dlgtitle,dims,definput)
 Interval=str2double(answer{1});
 PreBleachFrame=str2num(answer{2});
 RightAfterBleachframe=str2num(answer{3});
 FitRange=str2num(answer{4});
+
+list = {'With immobile fraction','Without immobile fraction'};
+[indx,tf] = listdlg('ListString',list,'SelectionMode','single');
+
+for file=1:size(listfile,1)
+inputimage=listfile(file).name;
+tiff_info = imfinfo(inputimage);
+  
 
 if FitRange>size(tiff_info, 1)
     FitRange=size(tiff_info, 1);
@@ -33,16 +40,18 @@ I0=FRAPData(RightAfterBleachframe ,3);
 t=( FRAPData(RightAfterBleachframe:FitRange, 1) - RightAfterBleachframe ) *Interval;
 y=  FRAPData(RightAfterBleachframe:FitRange, 3)./I1;
 
-list = {'With immobile fraction','Without immobile fraction'};
-[indx,tf] = listdlg('ListString',list,'SelectionMode','single');
+
 if indx==1
     F = @(x,xdata)x(3)*(1-x(1)*exp(-xdata/x(2)));
 elseif indx==2
     F = @(x,xdata)(1-x(1)*exp(-xdata/x(2)));
+
 end
 x0=[0.13 0.5 0.9];
 [x,resnorm,~,exitflag,output] = lsqcurvefit(F,x0,t,y);
-x
+disp(x)
+D=0.88*FRAPData(1,2)./pi ./ ( 4.0*x(2).*log(2));
+disp(D)
 p=plot( (FRAPData(:,1)-PreBleachFrame) * Interval, FRAPData(:,3) ./ I1,'*');
 hold on
 plot(t,F(x,t),'-k' )
@@ -57,15 +66,16 @@ ax.FontName='Arial';
 
 
 %%%check tif file
-figure
-colormap ('jet')
-tiff_info = imfinfo(inputimage); % return tiff structure, one element per image
-tiff_stack = imread(inputimage, 1) ; % read in first image
-%concatenate each successive tiff to tiff_stack
-for ii = 2 : size(tiff_info, 1)
-    temp_tiff = imread(inputimage, ii);
-    tiff_stack = cat(3 , tiff_stack, temp_tiff);
+% figure
+% colormap ('jet')
+% tiff_info = imfinfo(inputimage); % return tiff structure, one element per image
+% tiff_stack = imread(inputimage, 1) ; % read in first image
+% %concatenate each successive tiff to tiff_stack
+% for ii = 2 : size(tiff_info, 1)
+%     temp_tiff = imread(inputimage, ii);
+%     tiff_stack = cat(3 , tiff_stack, temp_tiff);
+% end
+% sliceViewer(tiff_stack);
+% colormap ('jet');
 end
-sliceViewer(tiff_stack);
-colormap ('jet');
 
