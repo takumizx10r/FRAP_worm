50aa636538690289433b467af7e4d5f6d1946912
takumizx10r <takumi.saitou.b2@tohoku.ac.jp>
2022-10-22 17:37:15 +0900

    Update

--- Gaussian_ListUpData_BoxPlot_compMolWeight.m
+++ Gaussian_ListUpData_BoxPlot_compMolWeight.m
@@ -23,33 +23,39 @@ for i=1:LengthFolder
     InputFileList=dir(strcat(InputFolderList{i},'\Gaussian-*.mat'));
 
     for j=1:length(InputFileList)
+        
         load(strcat(InputFileList(j).folder,'\',InputFileList(j).name), '-mat','DiffCoef','Fit_initial_Para');
+        
         if i==1 && j==1
             DiffusionCoefficient(j,1)=DiffCoef;
             Initial_parameters(j,1:3)=Fit_initial_Para;
             Index{j,1}=answer{1};
             M(j,1)=str2double(answer{2});
+            FolerList{j,1}=strcat(InputFileList(j).folder,'\',InputFileList(j).name);
         else
             DiffusionCoefficient=cat(1,DiffusionCoefficient,DiffCoef);
             Initial_parameters=cat(1,Initial_parameters,Fit_initial_Para);
             Index=cat(1,Index,answer{1});
             M=cat(1,M,str2double(answer{2}) );
+            FolerList=cat(1,FolerList,strcat(InputFileList(j).folder,'\',InputFileList(j).name));
         end
     end
     totalfile=totalfile+1;
 end
 Index=categorical(Index);
-T=table(DiffusionCoefficient,Initial_parameters,Index,M);
+T=table(DiffusionCoefficient,Initial_parameters,Index,M,FolerList);
 b=boxchart(categorical(T.Index),T.DiffusionCoefficient);
 b.BoxFaceColor='k';
 b.MarkerStyle='+';
-b.Notch='off'
+b.Notch='on';
 ax=gca; ax.FontSize=18; ax.FontName='Arial';
 % xlabel('Fluorophore')
 ylabel('Diffusion coefficient \fontname{Times}\itD\rm\fontname{Arial} (μm^2/s)');
 outputfolder=uigetdir(pwd);
-savefig(strcat(outputfolder,'\Result.fig'))
+savefig(strcat(outputfolder,'\Result.fig'));
 % exportgraphics(gcf,strcat(outputfolder,'\Result.png'),"Resolution",600);
+
+% [h_lillie,p_lillie,k_lillie,c_lillie] = lillietest(T.DiffusionCoefficient(T.Index=='Control'));
 
 % p_wilk=ranksum(T.DiffusionCoefficient(T.Index=='WT (N2)'),T.DiffusionCoefficient(T.Index=='et35'))
 
